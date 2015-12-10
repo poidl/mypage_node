161dcbb1e54049bb9af856a3ae681254f56f545d
poidl <hoitaus@gmail.com>
2015-12-10 17:02:24 +1100

    promises

--- servers/server.js
+++ servers/server.js
@@ -40,30 +40,86 @@ function respond(code, body, response) {
     response.end(body);
 }
 
-myget = function(path, respond, response) {
-  fs.stat(path, function(error, stats) {
-    if (error && error.code == "ENOENT")
-      respond(404, "File not found", response);
-    else if (error)
-      respond(500, error.toString());
-    else
-      respond(200, fs.createReadStream(path), response)
-              // require("mime").lookup(path));
-  });
-};
+// myget = function(path, respond, response) {
+//   fs.stat(path, function(error, stats) {
+//     if (error && error.code == "ENOENT")
+//       respond(404, "File not found", response);
+//     else if (error)
+//       respond(500, error.toString(),response);
+//     else
+//       respond(200, fs.createReadStream(path), response)
+//               // require("mime").lookup(path));
+//   });
+// };
 
+fs_stat_promise = function(path) {
+  var promise = new Promise( function(resolve, reject) {
+    fs.stat(path, function(error, stats) {
+      if (error)
+        reject(error);
+      else
+        resolve(stats);
+    });
+  });
+  return promise;
+}
+
+mygetnew = function(path, respond, response) {
+  fs_stat_promise(path).then( function(stats){
+      respond(200, fs.createReadStream(path), response)
+    })
+    .catch( function(error) {
+      response.writeHead(500);
+      response.end(error.toString());
+      console.log("Response failed: ", error.stack);
+    });
+}
+
+// function respondTo(request) {
+//   var path = require("url").parse(request.url).pathname;
+//   // requested is a framed page via pretty url
+//   if (obj=mymap.idx(path, "framed", "prettyurl")) {
+//     //AJAX - don't rebuild the frame
+//     // console.log(request.headers)
+//     if (request.headers["x-requested-with"] == 'XMLHttpRequest') {
+//       // console.log('AJAX request: '+obj.resource)
+//       return fs_stat_promise(path).then( function(stats){
+//         respond(200, fs.createReadStream(path), response)
+//       })
+//     } else {
+//       // console.log('Framing: '+obj.resource)
+//       // console.log('calling readFile '+obj.resource);
+//       fs.readFile(obj.resource, 'utf8', function(err,data_content) {
+//         if (err) throw err;
+//         // console.log('calling readFile /framed/frame.html');
+//         fs.readFile('./framed/frame.html', 'utf8', function(err,data_frame) {
+//           var rendered = mustache.render(data_frame, {content: data_content});
+//           // console.log(rendered)
+//           respond(200, rendered, response)
+//         });
+//       });
+//     }
+//   }
+//   // else if (obj=mymap.idx(path, "framed", "resource"))
+//   else {
+//     // for css and png ??
+//     console.log('Direct: '+path)
+//     mygetnew('./'+path, respond, response)
+//     //respond(404, "File not found", response);
+//   }
+// }
 
 http.createServer(function(request, response) {
   var path = require("url").parse(request.url).pathname;
   // requested is a framed page via pretty url
   if (obj=mymap.idx(path, "framed", "prettyurl")) {
     //AJAX - don't rebuild the frame
-    console.log(request.headers)
+    // console.log(request.headers)
     if (request.headers["x-requested-with"] == 'XMLHttpRequest') {
-      console.log('AJAX request: '+obj.resource)
-      myget(obj.resource,respond, response)
+      // console.log('AJAX request: '+obj.resource)
+      mygetnew(obj.resource,respond, response)
     } else {
-      console.log('Framing: '+obj.resource)
+      // console.log('Framing: '+obj.resource)
       // console.log('calling readFile '+obj.resource);
       fs.readFile(obj.resource, 'utf8', function(err,data_content) {
         if (err) throw err;
@@ -80,37 +136,7 @@ http.createServer(function(request, response) {
   else {
     // for css and png ??
     console.log('Direct: '+path)
-    myget('./'+path, respond, response)
+    mygetnew('./'+path, respond, response)
     //respond(404, "File not found", response);
   }
 }).listen(8080);
-
-
-// var http = require("http");
-// // The requestListener is a function which is automatically added to the 'request' event
-// var requestListener = function(request, response) {
-//     response.writeHead(200, {"Content-Type": "text/html"});
-//     urlregex = /^\/withFrame\/([^\/]+)$/;
-//     var path = require("url").parse(request.url).pathname;
-//     var repl = urlregex.exec(path);
-//     response.write("<h1>Hello!</h1>"+
-//     "<p>request.url <code>" + request.url + "</p>"+
-//     "<p>replace:  <code>" + repl + "</p>"+
-//     "<p>slice(1):  <code>" + repl.slice(1) + "</p>");
-//     response.end();
-// }
-//
-// var server = http.createServer(requestListener);
-// server.listen(8080);
-
-
-// var http = require("http");
-// http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type": "text/plain"});
-//   request.on("data", function(chunk) {
-//     response.write(chunk.toString().toUpperCase());
-//   });
-//   request.on("end", function() {
-//     response.end();
-//   });
-// }).listen(8080);
